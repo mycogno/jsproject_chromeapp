96437d68b199c64f8145603353e168039c4ce68e
mycogno <43112548+mycogno@users.noreply.github.com>
2020-02-09 20:52:51 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# js_project_making_chromeapp
+ chrome app with clock and to-do list
---
+++ index.js
@@ -0,0 +1,17 @@
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if (!hasClass) {
+    title.classList.add(CLICKED_CLASS);
+  } else {
+    title.classList.remove(CLICKED_CLASS);
+  }
+}
+
+function init() {
+  title.addEventListener("mouseenter", handleClick);
+}
+init();
